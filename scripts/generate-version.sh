f4c4177dbcf4298b2df6a153f51e22242aea40f5
Huan LI <zixia@zixia.net>
2019-06-10 15:54:40 +0800

    fix self redirect problem

--- scripts/generate-version.sh
+++ scripts/generate-version.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-[ -f src/version.ts ] || {
-  echo "src/version.ts not found"
+SRC_VERSION_TS_FILE='src/version.ts'
+
+[ -f ${SRC_VERSION_TS_FILE} ] || {
+  echo ${SRC_VERSION_TS_FILE}" not found"
   exit 1
 }
 
 VERSION=$(ts-node --files bin/pkg-jq -r .version)
 
-cat <<_SRC_
+cat <<_SRC_ > ${SRC_VERSION_TS_FILE}
 /**
  * This file was auto generated from scripts/generate-version.sh
  */
